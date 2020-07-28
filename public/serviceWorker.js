713884d5fdb11ac8d26945f64406b886dc0d6ff7
sangya2001 <sangyasherpa2058@gmail.com>
2020-07-28 07:59:00 +0545

    Finished PWA

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -17,10 +17,23 @@ self.addEventListener('install', (e) => {
 
 //Listen for requests
 self.addEventListener('fetch', (e) => {
-
+    e.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+            .catch(() => caches.match('offline.html')) 
+        })
+    )
 });
 
 //Activate the SW
 self.addEventListener('activate', (e) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME)
 
+    e.waitUntil(
+        caches.keys()
+        .then((cacheNames) => Promise.all(cacheNames.map(cacheName => {
+                if(!cacheWhiteList.includes(cacheName)) return caches.delete(cacheName)
+        }))))
 });
